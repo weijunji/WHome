8e1c9aad414f2a30d04eb32765aa2bb4f31c451b
Haruka <990628wjy@gmail.com>
2020-04-13 14:51:56 +0800

    change blog category

--- api/blog-category/controllers/blog-category.js
+++ api/blog-category/controllers/blog-category.js
@@ -5,4 +5,31 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    let query = ctx.query;
+    if (query._q) {
+      entities = await strapi.services['blog-category'].search(query);
+    } else {
+      entities = await strapi.services['blog-category'].find(query);
+    }
+
+    return entities.map(entity => {
+      const result = sanitizeEntity(entity, { model: strapi.models['blog-category'] })
+      delete result.blogs
+      return result
+    });
+  },
+  async findOne(ctx) {
+    if (ctx.params['0']) {
+      delete ctx.params['0']
+    }
+    let entity = await strapi.services['blog-category'].findOne(ctx.params);
+    if (!ctx.state.user || ctx.state.user.blocked) {
+      const blogs = entity.blogs;
+      entity.blogs = blogs.filter(blog => !blog.private);
+    }
+    return sanitizeEntity(entity, { model: strapi.models['blog-category'] });
+  }
+};
